397413c74866d05fce240340da9b4cd564fce2df
mahdihashemi <mahdi.hashemi@shawmedia.ca>
2015-12-14 23:12:00 -0500

    samba adding config

--- cookbooks/samba-shared/recipes/default.rb
+++ cookbooks/samba-shared/recipes/default.rb
@@ -6,3 +6,20 @@
 package 'samba' do
   action :install
 end
+service 'smb' do
+  action [:enable, :start]
+end
+directory '/opt/dukkonbot/shared' do
+  owner 'dukkonbot'
+  group 'dukkonbot'
+  mode '777'
+  action :create
+end
+
+template '/etc/samba/smb.conf' do
+  source 'smb.conf.erb'
+  owner 'root'
+  group 'root'
+  mode '0644'
+  notifies :restart, 'service[smb]', :immediately
+end
